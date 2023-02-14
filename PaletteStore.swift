7bdb9b74f224165eef99be4a52e92e1208b20127
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2023-02-15 00:30:50 +0200

    create palette store view model

---
+++ PaletteStore.swift
@@ -0,0 +1,85 @@
+//
+//  PaletteStore.swift
+//  EmojiArt (SwiftUI)
+//
+//  Created by AHMED GAMAL  on 14/02/2023.
+//
+struct Palette: Identifiable, Codable{
+    var name: String
+    var emojis: String
+    var id: Int
+    
+    fileprivate init(name: String, emojis: String, id: Int) {
+        self.name = name
+        self.emojis = emojis
+        self.id = id
+    }
+}
+
+import SwiftUI
+class PaletteStore: ObservedObject{
+    let name: String
+    @Published var palettes = [Palette]() {
+        didSet {
+            storeInUserDefaults()
+        }
+    }
+    private var userDefaultsKey: String {
+        "PaletteStore:" + name
+    }
+
+    init(named name: String) {
+        self.name = name
+        restoreFromUserDefaults()
+        if palettes.isEmpty {
+            insertPalette(named: "Vehicles", emojis: "🚙🚗🚘🚕🚖🏎🚚🛻🚛🚐🚓🚔🚑🚒🚀✈️🛫🛬🛩🚁🛸🚲🏍🛶⛵️🚤🛥🛳⛴🚢🚂🚝🚅🚆🚊🚉🚇🛺🚜")
+            insertPalette(named: "Sports", emojis: "🏈⚾️🏀⚽️🎾🏐🥏🏓⛳️🥅🥌🏂⛷🎳")
+            insertPalette(named: "Music", emojis: "🎼🎤🎹🪘🥁🎺🪗🪕🎻")
+            insertPalette(named: "Animals", emojis: "🐥🐣🐂🐄🐎🐖🐏🐑🦙🐐🐓🐁🐀🐒🦆🦅🦉🦇🐢🐍🦎🦖🦕🐅🐆🦓🦍🦧🦣🐘🦛🦏🐪🐫🦒🦘🦬🐃🦙🐐🦌🐕🐩🦮🐈🦤🦢🦩🕊🦝🦨🦡🦫🦦🦥🐿🦔")
+            insertPalette(named: "Animal Faces", emojis: "🐵🙈🙊🙉🐶🐱🐭🐹🐰🦊🐻🐼🐻‍❄️🐨🐯🦁🐮🐷🐸🐲")
+            insertPalette(named: "Flora", emojis: "🌲🌴🌿☘️🍀🍁🍄🌾💐🌷🌹🥀🌺🌸🌼🌻")
+            insertPalette(named: "Weather", emojis: "☀️🌤⛅️🌥☁️🌦🌧⛈🌩🌨❄️💨☔️💧💦🌊☂️🌫🌪")
+            insertPalette(named: "COVID", emojis: "💉🦠😷🤧🤒")
+            insertPalette(named: "Faces", emojis: "😀😃😄😁😆😅😂🤣🥲☺️😊😇🙂🙃😉😌😍🥰😘😗😙😚😋😛😝😜🤪🤨🧐🤓😎🥸🤩🥳😏😞😔😟😕🙁☹️😣😖😫😩🥺😢😭😤😠😡🤯😳🥶😥😓🤗🤔🤭🤫🤥😬🙄😯😧🥱😴🤮😷🤧🤒🤠")
+        }
+    }
+
+    
+    private func storeInUserDefaults() {
+        UserDefaults.standard.set(try? JSONEncoder().encode(palettes), forKey: userDefaultsKey)
+    }
+
+    private func restoreFromUserDefaults() {
+        if let jsonData = UserDefaults.standard.data(forKey: userDefaultsKey),
+           let decodedPalettes = try? JSONDecoder().decode(Array<Palette>.self, from: jsonData) {
+            palettes = decodedPalettes
+        }
+    }
+
+    
+    
+    
+    
+    // MARK: - Intent
+    
+    func palette(at index: Int) -> Palette {
+        let safeIndex = min(max(index, 0), palettes.count - 1)
+        return palettes[safeIndex]
+    }
+    
+    @discardableResult
+    func removePalette(at index: Int) -> Int {
+        if palettes.count > 1, palettes.indices.contains(index) {
+            palettes.remove(at: index)
+        }
+        return index % palettes.count
+    }
+    
+    func insertPalette(named name: String, emojis: String? = nil, at index: Int = 0) {
+        let unique = (palettes.max(by: { $0.id < $1.id })?.id ?? 0) + 1
+        let palette = Palette(name: name, emojis: emojis ?? "", id: unique)
+        let safeIndex = min(max(index, 0), palettes.count)
+        palettes.insert(palette, at: safeIndex)
+    }
+
+}
