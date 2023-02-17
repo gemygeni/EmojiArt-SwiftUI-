c2c2b201cb33e4561a7abd3e9b3628080331f72b
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2023-02-17 06:45:08 +0200

    create a go to menu to navigate to specific palette

--- EmojiArt (SwiftUI)/paletteChooser.swift
+++ EmojiArt (SwiftUI)/paletteChooser.swift
@@ -53,9 +53,34 @@
             AnimatedActionButton(title: "Delete", systemImage: "minus.circle") {
                 chosenPaletteIndex = store.removePalette(at: chosenPaletteIndex)
             }
-            
+            gotoMenu
 
         }
+        
+        var gotoMenu: some View {
+            Menu {
+                ForEach (store.palettes) { palette in
+                    AnimatedActionButton(title: palette.name) {
+                        if let index = store.palettes.index(matching: palette) {
+                            chosenPaletteIndex = index
+                        }
+                    }
+                }
+
+            } label: {
+                Label("Go To", systemImage: "text.insert")
+            }
+
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 
