b1c84f742bb54a87e940f6f89222f24618db3a5d
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2023-02-18 07:19:57 +0200

    add edit mode button to delete or move palette

--- EmojiArt (SwiftUI)/PaletteManager.swift
+++ EmojiArt (SwiftUI)/PaletteManager.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct PaletteManager: View {
     @EnvironmentObject var store : PaletteStore
-    
+    @State private var editMode: EditMode = .inactive
+
+    @Environment (\.colorScheme) private var colorScheme
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
         NavigationView {
             List {
@@ -18,21 +21,39 @@ struct PaletteManager: View {
                         VStack(alignment: .leading){
                             Text(palette.name)
                             Text(palette.emojis)
-                        }
-
+                        }.gesture(editMode == .active ? tap : nil)
+                        
                     }
+                }
+                .onDelete { indexset in
+                    store.palettes.remove(atOffsets: indexset)
+                }
+                .onMove { indexset, newOffset in
+                    store.palettes.move(fromOffsets: indexset, toOffset: newOffset)
+
                 }
             }
             .navigationTitle("Manage Palette")
             .navigationBarTitleDisplayMode(.inline)
-
+            .toolbar(content: {
+                EditButton()
+            })
+            .environment(\.editMode, $editMode)
         }
     }
+    
+    
+    var tap: some Gesture {
+        TapGesture().onEnded { }
+    }
+
 }
 
 struct PaletteManager_Previews: PreviewProvider {
     static var previews: some View {
         PaletteManager()
+            .previewDevice("iPhone 14 Pro Max")
             .environmentObject(PaletteStore(named: "preview"))
+            .preferredColorScheme(.light)
     }
 }
